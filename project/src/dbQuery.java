8c7b3e5129c47f6f8ecfe4ed690720df66ba6347
Sihwan Jung <jsh0211@gmail.com>
2014-11-26 00:37:26 -0500

    Fixing conflicts

--- project/src/dbQuery.java
+++ project/src/dbQuery.java
@@ -616,7 +616,7 @@ public class dbQuery {
 	// **************************
 	// VisitationRecordPanel.java
 	// **************************
-
+	
 	public static ResultSet ViewVisitation_fillDoctorTable(String userId, String type)
 	{
 		String query ="";
@@ -726,7 +726,7 @@ public class dbQuery {
 			query = "SELECT a.AppointmentDate, a.AppointmentLength, CONCAT(d.FirstName,' ',d.LastName) AS 'Doctor', "
 					+ "CONCAT(p.FirstName, ' ', p.LastName) AS 'Patient', IFNULL(v.DoctorComment,'') AS DoctorComment,"
 					+ " IFNULL(v.VisitReason,'') AS VisitReason, IFNULL(v.ProcedureFee,'') AS ProcedureFee, IFNULL(v.ProcedureName,'') AS ProcedureName, "
-					+ "MAX(IFNULL(v.EnteredDate,'')) AS EnteredDate, v.AppointmentID, a.DoctorID FROM appointment a "
+					+ "MAX(IFNULL(v.EnteredDate,'')) AS EnteredDate, a.AppointmentID, a.DoctorID FROM appointment a "
 					+ "LEFT JOIN visitationrecord v ON a.AppointmentID = v.AppointmentID "
 					+ "LEFT JOIN doctor d ON a.DoctorID = d.DoctorID LEFT JOIN patient p ON a.PatientID = p.PatientID ";
 					if (!userId.equals("")) 
@@ -866,6 +866,4 @@ public class dbQuery {
 		return rs;
 
 	}
-	
-	
 }
